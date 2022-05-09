c96c79a2cd2b2e34d4816df02fafb747e1f9e71e
Timon Buschendorf <timon.buschendorf@fh-bielefeld.de>
2022-05-09 10:04:15 +0200

    Creating test class with test method

--- mocking/src/test/java/GartenTest.java
+++ mocking/src/test/java/GartenTest.java
@@ -1,11 +1,5 @@
-import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
-import static org.powermock.api.mockito.PowerMockito.mock;
-import static org.powermock.api.mockito.PowerMockito.when;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assume.assumeTrue;
 
 public class GartenTest {
 
