349ef3f326ea6556175033b4d5c9c333ee4d8080
Timon Buschendorf <timon.buschendorf@fh-bielefeld.de>
2022-05-09 09:59:54 +0200

    Fix in GartenTest

--- mocking/src/test/java/GartenTest.java
+++ mocking/src/test/java/GartenTest.java
@@ -9,47 +9,10 @@ import static org.junit.Assume.assumeTrue;
 
 public class GartenTest {
 
-    Pflanze pflanze1;
-    Pflanze pflanze2;
-    Pflanze pflanze3;
-    Garten<Pflanze> garten;
 
-    @Before
-    public void setup() {
-        pflanze1 = mock(Pflanze.class);
-        pflanze2 = mock(Pflanze.class);
-        pflanze3 = mock(Pflanze.class);
-        garten = new Garten<>();
-        assumeTrue(garten.getGartenSize() == 0);
-        garten.addToGarten(pflanze1);
-        garten.addToGarten(pflanze2);
-        garten.addToGarten(pflanze3);
-        assumeTrue(garten.getGartenSize() == 3);
-    }
-
-    /**
-     * Checkt ob removeUnkraut() eine Liste zurueck gibt mit einem
-     * bestimmten Objekt
-     */
     @Test
-    public void testRemoveUnkrautCheckUnkrautListe() {
-        when(pflanze1.getUnkraut()).thenReturn(false);
-        when(pflanze2.getUnkraut()).thenReturn(true);
-        when(pflanze3.getUnkraut()).thenReturn(false);
-        List<Pflanze> unkrautGarten = garten.removeUnkraut();
-        assertEquals(unkrautGarten.get(0), pflanze2);
-    }
+    public void testRemoveUnkraut() {
 
-    /**
-     * Checkt ob die zurueck gelieferte Liste die korrekte groesse hat
-     */
-    @Test
-    public void testRemoveUnkrautCheckUnkrautGartenSize(){
-        when(pflanze1.getUnkraut()).thenReturn(true);
-        when(pflanze2.getUnkraut()).thenReturn(false);
-        when(pflanze3.getUnkraut()).thenReturn(true);
-        List<Pflanze> unkrautGarten = garten.removeUnkraut();
-        assertEquals(unkrautGarten.size(), 2);
     }
 
 
